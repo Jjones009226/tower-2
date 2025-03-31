ce12314dec2d169086a21e9d03c443f3ffd00906
joseph832 <jonesj59@montclair.edu>
2025-03-31 19:58:04 -0400

    week 2b

--- tower 2/Assets/BaseProjectille.cs
+++ tower 2/Assets/BaseProjectille.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BaseProjectille : MonoBehaviour
 {
+    public float speed = 0.5f;
+    public 
+        
     // Start is called before the first frame update
     void Start()
     {
